ae73fa1d81c3b174d633d86982e4d3ab9e8b6155
nsivatej <125730384+nsivatej@users.noreply.github.com>
2023-02-18 15:05:34 +0530

    Update streamlit_app.py
    Adding Csv from S3 and transform as CSV

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,4 +6,6 @@ streamlit.text('🥗kale Spinach & Rocket smoothie')
 streamlit.text('🐔Hard-boiled Free range Egg')
 streamlit.text('🥑🍞Avacado Spinach Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
 
