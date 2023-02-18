d2ae35be5a8b14dd28843a976032669a9b060af1
nsivatej <125730384+nsivatej@users.noreply.github.com>
2023-02-19 00:38:03 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,6 +36,7 @@ my_cur.execute("SELECT * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("the fruit load list contains:")
 streamlit.dataframe(my_data_rows)
+add_my_fruit = streamlit.multiselect("pick some fruits:", list(my_fruit_list.index))
 
 
 
