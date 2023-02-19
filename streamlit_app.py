52f20672ff3b10b3aaf041925639a3f97bf01c92
nsivatej <125730384+nsivatej@users.noreply.github.com>
2023-02-19 08:47:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,10 +40,12 @@ streamlit.dataframe(my_data_rows)
 streamlit.header("Add a Fruit")
 new_fruit = streamlit.text_input('Enter a new fruit:')
 if streamlit.button('Add Fruit'):
-    insert_query = "INSERT INTO fruit_load_list (fruit) VALUES ('{}')".format(new_fruit)
+    insert_query = my_cur.execute("INSERT INTO fruit_load_list (fruit) VALUES (%s)", (new_fruit,))
     my_cur.execute(insert_query)
     my_cnx.commit()
     streamlit.write("Thank you for adding the fruit: {}".format(new_fruit))
+    
+ 
 
 
 
